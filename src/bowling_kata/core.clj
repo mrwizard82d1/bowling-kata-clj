0d62c8f844b33b5ad874165cd7f272e4034f4630
Larry Jones <larry.jones@reveal-energy.com>
2020-05-14 21:41:39 -0500

    Rework code
    Previous commits worked fine; however, I was not very confident in
    incrementally adding features.
    
    This commit simplifies the code for now.

--- src/bowling_kata/core.clj
+++ src/bowling_kata/core.clj
@@ -1,17 +1,20 @@
 (ns bowling-kata.core)
 
 
-(defn score-roll [score]
-  (case score
-    ("-" "F") 0
-    score))
+;; (defn score-roll [score]
+;;   (case score
+;;     ("-" "F") 0
+;;     score))
 
 
-(defn score-rolls [rolls]
-  (when (and (first rolls) (second rolls))
-    (apply + (map score-roll rolls))))
+;; (defn score-rolls [rolls])
+;;   (when (and (first rolls) (second rolls))
+;;     (apply + (map score-roll rolls)))
 
 
-(defn score-frame [frame-number game]
-  (when-let [rolls (get-in game [frame-number :rolls])]
-    (score-rolls rolls)))
+(defn score-roll [roll]
+  (case roll
+      ("-" "F") 0
+      "/" 10
+      "X" 10
+      roll))
--- test/bowling_kata/core_test.clj
+++ test/bowling_kata/core_test.clj
@@ -2,36 +2,34 @@
   (:require [midje.sweet :refer :all]
             [bowling-kata.core :as bkc]))
 
+
 (fact "canary test"
   (+ 2 2) => 4)
+
 
 (fact "score roll"
     (bkc/score-roll "-") => 0
     (bkc/score-roll "F") => 0
-    (bkc/score-roll 4) => 4)
+    (bkc/score-roll 4) => 4
+    (bkc/score-roll "/") => 10
+    (bkc/score-roll "X") => 10)
 
 
-(fact "score rolls"
-    (bkc/score-rolls [5 3]) => 8
-    (bkc/score-rolls ["-" 1]) => 1
-    (bkc/score-rolls ["F" 2]) => 2
-    (bkc/score-rolls ["-" "-"]) => 0)
+;; (facts "correctly calculate open frames"
+;;   (fact "correctly calculate frame with gutter ball"
+;;     (bkc/score-frame 3 {3 {:rolls ["-" "-"]}}) => 0
+;;     (bkc/score-frame 7 {7 {:rolls ["-" 3]}}) => 3
+;;     (bkc/score-frame 4 {4 {:rolls [9 "-"]}}) => 9)
+;;   (fact "correctly calculate frame with foul ball"
+;;     (bkc/score-frame 3 {3 {:rolls ["F" "F"]}}) => 0
+;;     (bkc/score-frame 7 {7 {:rolls ["F" 3]}}) => 3
+;;     (bkc/score-frame 4 {4 {:rolls [9 "F"]}}) => 9)
+;;   (fact "correctly calculate unmarked frame"
+;;     (bkc/score-frame 2 {}) => nil
+;;     (bkc/score-frame 2 {2 {}}) => nil
+;;     (bkc/score-frame 2 {2 {:rolls []}}) => nil
+;;     (bkc/score-frame 2 {2 {:rolls [3]}}) => nil))
 
-
-(facts "correctly calculate open frames"
-  (fact "correctly calculate frame with gutter ball"
-    (bkc/score-frame 3 {3 {:rolls ["-" "-"]}}) => 0
-    (bkc/score-frame 7 {7 {:rolls ["-" 3]}}) => 3
-    (bkc/score-frame 4 {4 {:rolls [9 "-"]}}) => 9)
-  (fact "correctly calculate frame with foul ball"
-    (bkc/score-frame 3 {3 {:rolls ["F" "F"]}}) => 0
-    (bkc/score-frame 7 {7 {:rolls ["F" 3]}}) => 3
-    (bkc/score-frame 4 {4 {:rolls [9 "F"]}}) => 9)
-  (fact "correctly calculate unmarked frame"
-    (bkc/score-frame 2 {}) => nil
-    (bkc/score-frame 2 {2 {}}) => nil
-    (bkc/score-frame 2 {2 {:rolls []}}) => nil
-    (bkc/score-frame 2 {2 {:rolls [3]}}) => nil))
 
 (facts "correctly calculate spare frames")
 
